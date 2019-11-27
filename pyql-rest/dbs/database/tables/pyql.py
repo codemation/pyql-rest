29540483a65de7e421c01d7584a1e108d6561715
Codematoin <joshjamison1@gmail.com>
2019-11-28 00:14:29 +0100

    added pyql table config, to create pyql table in each DB added

---
+++ pyql-rest/dbs/database/tables/pyql.py
@@ -0,0 +1,24 @@
+
+def db_attach(server):
+    """
+        special table added to all dbs in server.data for id / state tracking
+    """
+    import uuid, time
+    for database in server.data:
+        db = server.data[database]
+        if not 'pyql' in db.tables:
+            db.create_table(
+            'pyql', [
+                ('uuid', str, 'UNIQUE'), 
+                ('database', str), 
+                ('lastModTime', float)
+                ]
+            'uuid'
+            )
+            db.tables['pyql'].insert({
+                'uuid': uuid.uuid1(),
+                'database': database,
+                'lastModTime': time.time()
+                })
+    pass # Enter db.create_table statement here
+            
