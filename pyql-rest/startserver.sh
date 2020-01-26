7a6195c13e830e0bd5ada699c1f4985ef535e24c
Codematoin <joshjamison1@gmail.com>
2020-01-26 23:58:22 +0100

    setting apache listen port as PYQL_PORT env

--- pyql-rest/startserver.sh
+++ pyql-rest/startserver.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 HOSTIP=$(cat /etc/hosts | grep $(hostname) | awk '{print $1}')
+sed -i 's/PYQL_PORT/'$PYQL_PORT'/g' /etc/nginx/sites-enabled/sites-available-pyql-rest
 sed -i 's/HOSTIP/'$HOSTIP'/g' /etc/nginx/sites-enabled/sites-available-pyql-rest
 service nginx start
 /opt/venv/bin/uwsgi --ini wsgi.ini
