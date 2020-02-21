09f88314002915c860670b023914656da220b065
Codematoin <joshjamison1@gmail.com>
2020-02-21 15:01:56 +0100

    using IP derived by hostname if localhost is provided as DB_HOST for mysql type DB

--- pyql-rest/dbs/database/database_db.py
+++ pyql-rest/dbs/database/database_db.py
@@ -32,9 +32,9 @@ def run(server):
         #try:
         if config['type'] == 'mysql':
             from mysql.connector import connect as connector
-            if config['DB_HOST'] == 'localhost':
+            if config['host'] == 'localhost':
                 import socket, os
-                config['DB_HOST'] = socket.gethostbyname(os.environ['HOSTNAME'])
+                config['host'] = socket.gethostbyname(os.environ['HOSTNAME'])
         else:
             import sqlite3.connect as connector
         server.data[database] = data.database(connector, **config)
