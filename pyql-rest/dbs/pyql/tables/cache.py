12016a84b8e693bce439988c6f84f482bce04282
Codematoin <joshjamison1@gmail.com>
2020-02-29 16:05:40 +0100

    improvments to cache table / app which provide a more ACID compliant approach to handling requests in-order

--- pyql-rest/dbs/pyql/tables/cache.py
+++ pyql-rest/dbs/pyql/tables/cache.py
@@ -2,9 +2,11 @@ def db_attach(server):
     db = server.data['pyql']
     db.create_table(
        'cache', [
-           ('id', str, 'UNIQUE NOT NULL'), 
-           ('type', str), 
-           ('txn', str)
+           ('id', str, 'UNIQUE NOT NULL'), # uuid of cached txn
+           ('tableName', str),
+           ('type', str), # insert / update / delete / transaction
+           ('timestamp', float), # time of txn 
+           ('txn', str) # boxy of txn
        ],
        'id'
     )
