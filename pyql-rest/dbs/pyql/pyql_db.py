f621994914628eee32fa3647ba1efa593e40d6a4
Codematoin <joshjamison1@gmail.com>
2020-03-31 09:31:41 +0200

    corrected pyql db location selection to be inline with k8s & standalone rest endpoint usage

--- pyql-rest/dbs/pyql/pyql_db.py
+++ pyql-rest/dbs/pyql/pyql_db.py
@@ -1,30 +1,12 @@
 # pyql - type sqlite3
 def run(server):
-    """
-    import sys, os
-    @server.route('/pyql_attach')
-    def pyql_attach():
-        config=dict()
-            
-        with open('.cmddir', 'r') as projDir:
-            for projectPath in projDir:
-                config['database'] = f'{projectPath}dbs/pyql/pyql'
-        #USE ENV PATH for PYQL library or /pyql/
-        sys.path.append('/pyql/' if os.getenv('PYQL_PATH') == None else os.getenv('PYQL_PATH'))
-        try:
-            import data, sqlite3
-            from . import setup
-            server.data['pyql'] = data.database(sqlite3.connect, **config)
-            setup.attach_tables(server)
-            return {"status": 200, "message": "pyql attached successfully"}, 200
-        except Exception as e:
-            return {"status": 200, "message": repr(e)}, 500
-    pyql_attach()
-    """
+    import os
+    from pyql import data
+    import sqlite3
+    from . import setup
 
-    import sys, os
     log = server.log
-    @server.route('/internal/db/attach')
+    @server.route('/internal/db/attach') # TODO - add @server.isauthenicated 
     def pyql_attach():
         config=dict()
         if 'PYQL_TYPE' in os.environ:
@@ -35,19 +17,11 @@ def run(server):
             with open('.cmddir', 'r') as projDir:
                 for projectPath in projDir:
                     config['database'] = f'{projectPath}dbs/pyql/pyql'
-        #USE ENV PATH for PYQL library or /pyql/
-        #sys.path.append('/pyql/' if os.getenv('PYQL_PATH') == None else os.getenv('PYQL_PATH'))
-        #try:
-        from pyql import data
-        import sqlite3
-        from . import setup
         log.info("finished imports")
-        server.data[dbname] = data.database(sqlite3.connect, **config)
+        server.data['pyql'] = data.database(sqlite3.connect, **config)
         log.info("finished dbsetup")
         setup.attach_tables(server)
         log.info("finished attach_tables")
-        return {"status": 200, "message": f"database {dbname} attached successfully"}, 200
-        #except Exception as e:
-        #    return {"status": 200, "message": repr(e)}, 500
+        return {"status": 200, "message": f"database pyql attached successfully"}, 200
     response = pyql_attach()
     log.warning(response)
