e3a9ec6ad4999a20b5c6ffd4dc5ebeee2776e64c
Codematoin <joshjamison1@gmail.com>
2020-05-01 00:09:19 +0200

    corrected select_func issuing parsing parameters

--- pyql-rest/apps/select/select.py
+++ pyql-rest/apps/select/select.py
@@ -4,33 +4,10 @@ def run(server):
     log = server.log
     @server.route('/db/<database>/table/<table>/select', methods=['GET', 'POST'])
     @server.is_authenticated('local')
-    def select_func(database,table):
+    def cluster_select_func(database, table):
+        return select_func(database, table)
+    def select_func(database,table, params=None):
         message, rc = server.check_db_table_exist(database,table)
-        """
-        if rc == 200:
-            if request.method == 'GET':
-                response = server.data[database].tables[table].select('*')
-                return {"status": 200, "data": response}, 200
-            else:
-                params = request.get_json()
-                if 'select' in params:
-                    p = {}
-                    select = params['select']
-                    if 'join' in params:
-                        p['join'] = params['join']
-                    if 'where' in params:
-                        p['where'] = params['where']
-                    response = server.data[database].tables[table].select(*select, **p)
-                    return {"data": response}, 200
-                else:
-                    warning = f"table {table} select - missing selection"
-                    log.warning(warning)
-                    return {"warning": warning}, 400
-        else: 
-            return message, rc
-        
-                message, rc = server.check_db_table_exist(database,table)
-        """
         if not rc == 200:
             return {"error": f"received non 200 rec with message {message}, rc {rc} during check_db_table_exist"}, 500
         if request.method == 'GET' and params == None:
