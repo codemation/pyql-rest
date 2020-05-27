10543cf34783af1000523b26788a93fc47956638
Codematoin <joshjamison1@gmail.com>
2020-05-27 10:28:42 +0200

    enabled consistency for tables by default

--- pyql-rest/apps/rest/rest.py
+++ pyql-rest/apps/rest/rest.py
@@ -51,7 +51,8 @@ def run(server):
                     'uuid': dbuuid
                 },
                 "tables": tables
-            }
+            },
+            "consisteny": tables # TODO - add to environ variable
         }
         if 'PYQL_CLUSTER_JOIN_TOKEN' in os.environ and not os.environ['PYQL_CLUSTER_ACTION'] == 'test':
             joinClusterJob['joinToken'] = os.environ['PYQL_CLUSTER_JOIN_TOKEN']
