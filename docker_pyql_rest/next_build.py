1cbdbe73197c946c1fdc818be1fa2d9d71363912
Codematoin <joshjamison1@gmail.com>
2020-06-10 23:57:12 +0200

    added for pipeline usage in determining next build tag

--- docker_pyql_rest/next_build.py
+++ docker_pyql_rest/next_build.py
@@ -1,7 +1,11 @@
 import sys
 
 if __name__ == '__main__':
-    tag, latest = sys.argv[1], sys.argv[2]
-    tagId = float(latest.split(tag)[1])
-    tagId+=0.01
+    tag = sys.argv[1]
+    if len(sys.argv) == 2:
+        tagId = 0.01
+    else:
+        latest = sys.argv[2]
+        tagId = float(latest.split(tag)[1])
+        tagId+=0.01
     print(f"{tag}{tagId:.2f}")
