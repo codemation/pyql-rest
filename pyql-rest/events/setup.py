063612d98ebf0e238b8646615ca1db4ea2ef6c17
Codematoin <joshjamison1@gmail.com>
2020-06-11 15:30:32 +0200

    events/jobs will only start if PYQL_TYPE is K8s

--- pyql-rest/events/setup.py
+++ pyql-rest/events/setup.py
@@ -1,5 +1,7 @@
 def run(server):
+    import os
     from events.health import health
     health.run(server)
-    from events.jobs import jobs
-    jobs.run(server)
+    if os.environ.get('PYQL_TYPE') == 'K8S':
+        from events.jobs import jobs
+        jobs.run(server)
