a5ef35c481e6fcc4289100ad14ae6e9961a9b258
Codematoin <joshjamison1@gmail.com>
2020-09-28 14:43:26 +0200

    added probe, headers & sessions mgr methods from pyql-cluster-fastpi

--- pyql-rest/apps/rest/rest.py
+++ pyql-rest/apps/rest/rest.py
@@ -6,6 +6,121 @@ async def run(server):
     log = server.log
 
     os.environ['HOSTNAME'] = '-'.join(os.environ['PYQL_NODE'].split('.'))
+
+    server.sessions = {}
+
+    #@server.trace
+    async def get_endpoint_sessions(endpoint, **kw):
+        """
+        pulls endpoint session if exists else creates & returns
+        """
+        trace = log.warning
+        loop = server.event_loop if not 'loop' in kw else kw['loop']
+        async def session():
+            async with ClientSession(loop=loop) as client:
+                trace(f"started session for endpoint {endpoint}")
+                while True:
+                    status = yield client
+                    if status == 'finished':
+                        trace(f"finished session for endpoint {endpoint}")
+                        break
+        if not endpoint in server.sessions:
+            server.sessions[endpoint] = [{'session': session(), 'loop': loop}]
+            return await server.sessions[endpoint][0]['session'].asend(None)
+        for client in server.sessions[endpoint]:
+            if loop == client['loop']:
+                return await client['session'].asend(endpoint)
+        trace("session existed but not for this event loop, creating")
+        client = session()
+        server.sessions[endpoint].append({'session': client, 'loop': loop})
+        return await client.asend(None)
+
+    # attaching to global server var    
+    server.get_endpoint_sessions = get_endpoint_sessions
+
+    # creating initial session for this nodeq
+    await get_endpoint_sessions(node_id)
+
+    async def cleanup_session(endpoint):
+        #try:
+        if endpoint in server.sessions:
+            log.warning(f"removing session for endpoint {endpoint}")
+            try:
+                for session in server.sessions[endpoint]:
+                    try:
+                        await session['session'].asend('finished')
+                    except StopAsyncIteration:
+                        pass
+                del server.sessions[endpoint]
+            except Exception as e:
+                log.exception("exception when cleaning up session")
+        #except StopAsyncIteration:
+        #    await cleanup_session(endpoint)
+        #    del server.sessions[endpoint]
+        #return
+    async def cleanup_sessions():
+        for endpoint in server.sessions:
+            await cleanup_session(endpoint)
+        return
+
+    #@server.trace
+    async def get_auth_http_headers(location=None, token=None, **kw):
+        trace = log
+        if token == None:
+            auth = 'PYQL_CLUSTER_SERVICE_TOKEN' if not location == 'local' else 'PYQL_LOCAL_SERVICE_TOKEN'
+            trace.warning(f"get_auth_http_headers called using location: {location} - token: {token} - {kw} auth: {auth}")
+            token = kw['token'] if 'token' in kw else None
+            token = await server.env[auth] if token == None else token
+        headers = {
+            'Accept': 'application/json', "Content-Type": "application/json",
+            "authentication": f"Token {token}"}
+        trace.warning(f"get_auth_http_headers {headers}")
+        return headers
+
+    #@server.trace
+    async def probe(path, method='GET', data=None, timeout=5.0, auth=None, headers=None, **kw):
+        trace = log
+        auth = 'PYQL_CLUSTER_SERVICE_TOKEN' if not auth == 'local' else 'PYQL_LOCAL_SERVICE_TOKEN'
+        headers = await get_auth_http_headers(auth, **kw) if headers == None else headers
+        session, temp_session, temp_id = None, False, None
+        loop = asyncio.get_running_loop() if not 'loop' in kw else kw['loop']
+
+        if not 'session' in kw:
+            temp_session, temp_id = True, str(uuid.uuid1())
+            session = await get_endpoint_sessions(temp_id, loop=loop)
+        else:
+            session = kw['session']
+            
+        url = f'{path}'
+        try:
+            request = {
+                'probe': {
+                    'path': url,
+                    'headers': headers,
+                    'timeout': timeout
+                }
+            }
+            if method == 'GET':
+                result = await async_get_request(session, request, loop=loop) 
+            else:
+                request['probe']['data'] = data
+                result = await async_post_request(session, request, loop=loop)
+            result, status = result['probe']['content'], result['probe']['status']
+        except Exception as e:
+            error = f"Encountered exception when probing {path} - {repr(e)}"
+            result, status = {"error": trace.error(error)}, 500
+        trace.info(f"request: {request} - result: {result}")
+        if temp_session:
+            await cleanup_session(temp_id)
+        return result, status
+    server.probe = probe
+
+
+
+
+
+
+
     # check pyql tables & create join jobs to join cluster
     for db in server.data:
         if db == 'pyql':
@@ -75,12 +190,12 @@ async def run(server):
         return await node_reset_cache(reason,  request=await server.process_request(request))
 
     @server.is_authenticated('local')
-    @server.trace
+    #@server.trace
     async def node_reset_cache(reason, **kw):
         """
             resets local db table 'cache' 
         """
-        trace=kw['trace']
+        trace=log.warning
         trace(f"cache reset called for {reason}")
         await server.reset_cache()
         return {"message": f"{nodeId} reset_cache completed"} 
